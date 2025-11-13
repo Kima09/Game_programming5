cc90f4492af383ed7487a159df5c0312654e63b7
Kima09 <hkdavidl666@gmail.com>
2025-11-13 19:41:45 +0900

    (2025.11.13) 거품 정렬

---
+++ Class3rd (Double Sort)/Source.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 인플레이스 정렬
+	//정렬 과정에서 추가적인 자료구조 또는 메모리 공간을
+	//많이 사용하지 않고, 원래 주어진 배열이나 리스트 자체를
+	//직접 수정하여 정렬하는 방식입니다.
+
+#pragma endregion
+
+#pragma region 거품 정렬
+	//서로 인접한 두 원소를 비교하여 정렬하는 알고리즘입니다.
+
+	int list[] = { 3,5,6,1,2,7,4 };
+	int size = sizeof(list) / sizeof(list[0]);
+
+	for (int i = 0; i < size - 1;i++)
+	{
+		for (int j = 0; j < (size - i) - 1;j++)
+		{
+			if (list[j] > list[j + 1])
+			{
+				std::swap(list[j], list[j + 1]);
+			}
+		}
+	}
+
+	for (const auto& element : list)
+	{
+		cout << element << " ";
+	}
+
+#pragma endregion
+
+
+
+	return 0;
+}
---
+++ Class4th (Select Sort)/Class4th (Select Sort).cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 선택 정렬
+	//주어진 리스트 중에서 최소값을 찾은 다음 특정한 위치에서
+	//결과를 교체하는 방식으로 정렬하는 알고리즘입니다.
+
+	int list[] = { 9,6,7,3,5 };
+	int size = sizeof(list) / sizeof(list[0]);
+
+	for (int i = 0;i < size - 1;i++)
+	{
+		int min = list[i];
+		int index = i;
+		for (int j = i + 1; j < size;j++)
+		{
+			if (min > list[j])
+			{
+				min = list[j];
+				index = j;
+			}
+		}
+		std::swap(list[i], list[index]);
+	}
+
+	for (const auto& element : list)
+	{
+		cout << element << " ";
+	}
+
+	//시간복잡도는 더블 소트처럼 n제곱이다. 실질적으로 더블 소트보다는 약간 더 빠르다.
+#pragma endregion
+
+
+	return 0;
+}

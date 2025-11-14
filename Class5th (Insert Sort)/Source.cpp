f58d1192237c6dc7394553a11c1bbc14d2cd032c
Kima09 <hkdavidl666@gmail.com>
2025-11-14 19:44:17 +0900

    (2025.11.14) 선택 정렬

---
+++ Class5th (Insert Sort)/Source.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+#pragma region 삽입 정렬
+	//데이터를 하나씩 확인하면서 이미 정렬된 부분과 비교하여
+	//자신의 위치를 찾아 삽입하는 방식으로 정렬하는 알고리즘입니다.
+
+	//스왑 함수는 사용해선 안 된다.
+
+	int list[] = { 8,5,6,2,4 };
+	int size = sizeof(list) / sizeof(list[0]);
+
+	for (int i = 1;i < size;i++)
+	{
+		int key = list[i];
+		int j = i - 1;
+		for (; j >= 0 && list[j] > key;j--)
+		{
+			list[j + 1] = list[j];
+		}
+		list[j + 1] = key;
+	}
+
+	for (const auto& element : list)
+	{
+		cout << element << " ";
+	}
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Program.cpp
+++ Program/Program.cpp
@@ -4,6 +4,50 @@ using namespace std;
 
 int main()
 {
+#pragma region 유클리드 호제법
+//2개의 자연수 또는 정식의 최대 공약수를 구하는 방식으로
+//두 수가 서로 상대방 수를 나누어서 원하는 수를 얻어내는 알고리즘입니다.
+
+	//int list[] = {24, 36};
+	//int res = 1;
+	//int pow = 1;
+	//while (pow == 1)
+	//{
+	//	pow = 0;
+	//	for (int i = 2;i <= list[0] && i <= list[1];i++)
+	//	{
+	//		if (list[0] % i == 0 && list[1] % i == 0)
+	//		{
+	//			res = res * i;
+	//			list[0] = list[0] / i;
+	//			list[1] = list[1] / i;
+	//			pow = 1;
+	//			break;
+	//		}
+	//	}
+	//	if (pow == 0)
+	//	{
+	//		break;
+	//	}
+	//}
+	//cout << res << endl;
+
+	int x = 2304;
+	int y = 1440;
+	int result = 1;
+	for (int i = 1;i <= x && i <= y;i++)
+	{
+		if (x % i == 0 && y % i == 0)
+		{
+			result = result = i;
+		}
+	}
+	cout << "Greatest Common Divisor: " << result << endl;
+	//시간 복잡도: n
+	//로그 n으로 줄이기 위해 유클리드 호제법 사용
+	
+#pragma endregion
+
 
 	return 0;
 }

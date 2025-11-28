869eb8b1474e79b2d65e9b31f6ffd6edb3e95cdc
Kima09 <hkdavidl666@gmail.com>
2025-11-28 19:46:00 +0900

    (2025.11.28) 너비 우선 탐색

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -12,13 +12,63 @@ class Graph
 private:
 	unordered_map<T, vector<T>> adjacencyList;
 	unordered_map<T, int> degree;
+	unordered_set<T> vertices;
 public:
 	void insert(const T& i, const T& j)
 	{
 		adjacencyList[i].push_back(j);
 		degree[j]++;
+		vertices.insert(i);
+		vertices.insert(j);
+
+		if (degree.count(i) == false)
+		{
+			degree[i] = 0;
+		}
 	}
-	
+	void sort()
+	{
+		queue<T> queue;
+
+		for (const T& i: vertices)
+		{
+			if (degree[i] == 0)
+			{
+				queue.push(i);
+			}
+		}
+
+		//if (queue.empty())
+		//{
+		//	cout << "Cycle has occured" << endl;
+		//}
+
+		while (!queue.empty())
+		{
+			T i = queue.front();
+			for (const T& c : adjacencyList[i])
+			{
+				degree[c] -= 1;
+				if (degree[c] == 0)
+				{
+					queue.push(c);
+				}
+			}
+			
+			cout << i << " ";
+
+			queue.pop();
+
+			//하단: 사이클 확인
+			vertices.erase(i);
+			//vertices의 노드까지 지우는 코드, 사이클 확인 시 사용
+			if (!vertices.empty() && queue.empty())
+			{
+				cout <<endl << "Cycle has occured" << endl;
+				break;
+			}
+		}
+	}
 };
 
 int main()
@@ -38,6 +88,18 @@ int main()
 	//2. 큐에서 원소를 꺼내 연결된 모든 간선을 제거
 	//3. 간선 제거 이후 진입 차수가 0이 된 정점을 큐에 삽입
 	//4. 큐가 비어있을 때까지 2-3번 작업 반복
+
+	Graph<int> graph;
+	
+	graph.insert(7, 3);
+
+	graph.insert(3, 4);
+
+	graph.insert(4, 6);
+
+	graph.insert(6, 3);
+
+	graph.sort();
 #pragma endregion
 
 
